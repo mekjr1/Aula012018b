ff30d6538169e304bdd44e48b55d286ad3600de9
mekjr1 <gafar_meque@hotmail.com>
2018-08-21 11:48:23 +0200

    Initial Commit

---
+++ app/src/main/java/ucm/ac/mz/aula012018b/MainActivity.java
@@ -0,0 +1,13 @@
+package ucm.ac.mz.aula012018b;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
